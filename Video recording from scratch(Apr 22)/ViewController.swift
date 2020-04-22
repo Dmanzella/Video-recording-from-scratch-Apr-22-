12d4596a3dde2643bc5fcda015306bf71409f000
Dmanzella <58736612+Dmanzella@users.noreply.github.com>
2020-04-22 14:32:09 -0500

    Initial Commit

---
+++ Video recording from scratch(Apr 22)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video recording from scratch(Apr 22)
+//
+//  Created by Thomas Manzella on 4/22/20.
+//  Copyright © 2020 Derek Manzella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
