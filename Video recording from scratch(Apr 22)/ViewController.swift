3bd385372ab311f4296adb7fa9cd54ca9ac8859d
Dmanzella <58736612+Dmanzella@users.noreply.github.com>
2020-05-08 11:46:27 -0500

    Video App (works)

--- Video recording from scratch(Apr 22)/ViewController.swift
+++ Video recording from scratch(Apr 22)/ViewController.swift
@@ -2,19 +2,78 @@
 //  ViewController.swift
 //  Video recording from scratch(Apr 22)
 //
-//  Created by Thomas Manzella on 4/22/20.
+//  Created by Derek Manzella on 4/22/20.
 //  Copyright © 2020 Derek Manzella. All rights reserved.
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var controller = UIImagePickerController()
+    let videoFileName = "/video.mp4"
+    
+    
+    
+    @IBAction func takeVideo(_ sender: Any) {
 
-
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                
+            controller.sourceType = .camera
+            controller.mediaTypes = [kUTTypeMovie as String]
+            controller.delegate = self
+                
+            present(controller, animated: true, completion: nil)
+        }
+        else {
+            print("Camera is not available")
+        }
+    }
+    
+    
+    @IBAction func viewLibrary(_ sender: Any) {
+    controller.sourceType = UIImagePickerController.SourceType.photoLibrary
+        controller.mediaTypes = [kUTTypeMovie as String]
+        controller.delegate = self
+            
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 1
+        if let selectedVideo:URL = (info[UIImagePickerController.InfoKey.mediaURL] as? URL) {
+            // Save video to the main photo album
+            let selectorToCall = #selector(ViewController.videoSaved(_:didFinishSavingWithError:context:))
+              
+            // 2
+            UISaveVideoAtPathToSavedPhotosAlbum(selectedVideo.relativePath, self, selectorToCall, nil)
+            // Save the video to the app directory
+            let videoData = try? Data(contentsOf: selectedVideo)
+            let paths = NSSearchPathForDirectoriesInDomains(
+                FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+            let documentsDirectory: URL = URL(fileURLWithPath: paths[0])
+            let dataPath = documentsDirectory.appendingPathComponent(videoFileName)
+            try! videoData?.write(to: dataPath, options: [])
+        }
+        // 3
+        picker.dismiss(animated: true)
+    }
+    
+    @objc func videoSaved(_ video: String, didFinishSavingWithError error: NSError!, context: UnsafeMutableRawPointer){
+        if let theError = error {
+            print("error saving the video = \(theError)")
+        } else {
+           DispatchQueue.main.async(execute: { () -> Void in
+           })
+        }
+    }
+    
+    
+    
 }
 
